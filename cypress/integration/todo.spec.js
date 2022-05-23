db6de822bcfaebfe8c5cf6ec60053bcc839f3ac2
chackett1 <Chriscph1@gmail.com>
2022-05-23 16:36:16 -0400

    Add New Hero Test

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,5 +1,18 @@
 describe('My First Test', () => {
+  
   it('Does not do much!', () => {
     expect(true).to.equal(true)
+  })
+
+  it('Add New Hero', () => {
+    cy.visit('http://localhost:4200/dashboard', { timeout: 120000 })
+
+    cy.get('a[routerlink="/heroes"]').click()
+
+    cy.get('input[id="new-hero"]').type('New Hero')
+
+    cy.contains('button', 'Add hero').click()
+
+    cy.contains('New Hero')
   })
 })
